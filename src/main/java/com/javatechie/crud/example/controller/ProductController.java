4fd2054a3a5f6e3acc572052590d7140543dc306
Athul <athulkkuniyan@gmail.com>
2024-12-15 02:28:09 +0530

    first commit

--- src/main/java/com/javatechie/crud/example/controller/ProductController.java
+++ src/main/java/com/javatechie/crud/example/controller/ProductController.java
@@ -13,38 +13,19 @@ public class ProductController {
     @Autowired
     private ProductService service;
 
-    @PostMapping("/addProduct")
-    public Product addProduct(@RequestBody Product product) {
-        return service.saveProduct(product);
-    }
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "Service is up and running!";
 
-    @PostMapping("/addProducts")
-    public List<Product> addProducts(@RequestBody List<Product> products) {
-        return service.saveProducts(products);
     }
 
     @GetMapping("/products")
-    public List<Product> findAllProducts() {
+    public List<Product> getAllProducts() {
         return service.getProducts();
     }
+    @GetMapping("/products/search")
+    public List<Product> searchProducts(@RequestParam String keyword) {
+    return service.searchProductsByKeyword(keyword);
+}
 
-    @GetMapping("/productById/{id}")
-    public Product findProductById(@PathVariable int id) {
-        return service.getProductById(id);
-    }
-
-    @GetMapping("/product/{name}")
-    public Product findProductByName(@PathVariable String name) {
-        return service.getProductByName(name);
-    }
-
-    @PutMapping("/update")
-    public Product updateProduct(@RequestBody Product product) {
-        return service.updateProduct(product);
-    }
-
-    @DeleteMapping("/delete/{id}")
-    public String deleteProduct(@PathVariable int id) {
-        return service.deleteProduct(id);
-    }
 }
